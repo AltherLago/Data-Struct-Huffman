a7797f4a3e776f64003d958ef7655be004e8f98d
Ramon Basto <42789151+ramonbcll@users.noreply.github.com>
2020-01-15 03:04:01 -0300

    Update compress.h

--- Test/Headers/compress.h
+++ Test/Headers/compress.h
@@ -5,9 +5,5 @@
 #include "hash.h"
 
 void frequency(FILE *file, int amount[]);
-void get_trash(node *tree, unsigned char *trash, int height);
-void add_tree(node *tree, FILE *file);
-unsigned char set_bit(unsigned char byte, int i);
-void write_file(FILE *file, hash *hash, FILE *compressed, long long int sizeTree);
 
 #endif
