3d1e162c8e5989a0d99590f39b3583f01c587a48
Ramon Basto <42789151+ramonbcll@users.noreply.github.com>
2020-01-30 23:29:10 -0300

    Update descompress.h

--- Data-Struct-Huffman-master/Test/Headers/descompress.h
+++ Data-Struct-Huffman-master/Test/Headers/descompress.h
@@ -17,8 +17,12 @@ void read_header(FILE *in_file, unsigned short *size_tree, unsigned short *size_
 
 node_tree *new_node(unsigned short charac, node_tree *left, node_tree *right);
 
-node_tree *build_tree(unsigned short tree_array, unsigned short size_tree);
+node_tree *get_tree(unsigned char tree_array[], unsigned short size_tree, int *i, node_tree *tree);
 
-unsigned short get_tree(FILE *in_file, unsigned short size_tree);
+void tree_array(FILE *in_file, unsigned short size_tree, unsigned char *tree_array[]);
+
+void print_dec_tree(node_tree *node);
+
+int isDempty(node_tree *tree);
 
 #endif
